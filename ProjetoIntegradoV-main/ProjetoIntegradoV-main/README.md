fba8d5433528d1613961a03514fcf5f87a8de5a1
SahLilith <165191496+SahLilith@users.noreply.github.com>
2024-05-17 10:33:36 -0300

    att.dinosfauro-maquina-combate

---
+++ ProjetoIntegradoV-main/ProjetoIntegradoV-main/README.md
@@ -0,0 +1,1 @@
+Projeto Integrado V: Sabrina , Kaun , Outro Pedro, Bianca, Nicoly e Vitor
---
+++ ProjetoIntegradoV-main/ProjetoIntegradoV-main/html/js/bemvindo.js
@@ -0,0 +1,9 @@
+// Função para exibir o popup quando a página é carregada
+window.onload = function() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+// Função para fechar o popup
+function fecharPopup() {
+    document.getElementById('popup').style.display = 'none';
+}
